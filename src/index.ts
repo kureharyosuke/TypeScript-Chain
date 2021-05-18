65d6ddb332d60d226e01cc62cc539e820ba97a60
kureharyosuke <ohhayo.com@gmail.com>
2021-05-18 22:41:30 +0900

    add: interface

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-const name = "Oh",
-  age = 30,
-  gender = "male";
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
 
+const person = {
+  name: "kureha",
+  gender: "male",
+  age: 22,
+};
 const TsFunc = (name: string, age: number, gender?: string): void => {
   console.log(`Hello, ${name}, you are ${age}, you are a ${gender}`);
 };
 
-const TsFuncRe = (name: string, age: number, gender?: string): string => {
-  return `Hello, ${name}, you are ${age}, you are a ${gender}`;
+const TsFuncRe = (person: Human): string => {
+  return `Hello, ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
 // TsFunc(name, age); // 타입스크립트 파라미터 부족하면 에러발생
